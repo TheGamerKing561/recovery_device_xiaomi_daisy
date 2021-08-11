a384e9ce8871235eec15078171377533199ea101
ItsVixano <vixanonew@gmail.com>
2021-08-12 02:59:22 +0300

    daisy: Disable audit logging
    (original commit: https://github.com/twrp-daisy/twrp_device_xiaomi_daisy/commit/6e1f901d1658d083c8bcf91f3fb6f621162fac17)
    
    * It is useless because TWRP sets SELinux to permissive anyways
    
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Signed-off-by: TheGamerKing561 <eyadtamer2121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ BOARD_PROVIDES_BOOTCTRL := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 loop.max_part=7 androidboot.boot_devices=soc/7824900.sdhci
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 loop.max_part=7 androidboot.boot_devices=soc/7824900.sdhci audit=0
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
